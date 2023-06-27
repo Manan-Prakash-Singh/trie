82024a06d86c12ba19a2b9ab0358d46ad76c3ddc
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-06-28 00:50:34 +0530

    added search

--- trie.c
+++ trie.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,20 +11,19 @@ typedef struct trie_struct trie_t;
 
 struct trie_struct {
 	trie_t *arr[26];
-	int is_terminal;
 	int val;
 };
 
 trie_t *create_trie();
 trie_t *destroy_trie();
-int insert_key(trie_t *t, const char *key, int val);
+void insert_key(trie_t *t, const char *key, int val);
+int search(trie_t *t, const char *key);
 
 int main(int argc, char **argv) {}
 
 trie_t *create_trie() {
 
 	trie_t *t = malloc(sizeof(*t));
-	t->is_terminal = 0;
 	t->val = 0;
 
 	for (int i = 0; i < 26; i++) {
@@ -33,18 +33,14 @@ trie_t *create_trie() {
 	return t;
 }
 
-int insert_key(trie_t *t, const char *key, int val) {
-	if (!t) {
-		fprintf(stderr, "null value for trie\n");
-		return -1;
-	}
-
+void insert_key(trie_t *t, const char *key, int val) {
+	assert(t != NULL && "null value for the trie");
 	int len = strlen(key);
 
 	trie_t *cur_trie = t;
 
 	for (int i = 0; i < len; i++) {
-		int c = key[i];
+		int c = key[i] - 'a';
 		if (!cur_trie->arr[c]) {
 			cur_trie->arr[c] = create_trie();
 		}
@@ -52,6 +48,25 @@ int insert_key(trie_t *t, const char *key, int val) {
 		cur_trie = cur_trie->arr[c];
 	}
 
-	cur_trie->is_terminal = 1;
-	cur_trie->val = val;
+	cur_trie->val++;
+}
+
+int search(trie_t *t, const char *key) {
+	assert(t != NULL && "Null trie given.");
+
+	int len = strlen(key);
+
+	trie_t *itr = t;
+	trie_t *prev = t;
+
+	for (int i = 0; i < len; i++) {
+		int c = key[i] - 'a';
+		if (!itr->arr[c]) {
+			return 0;
+		}
+		prev = itr;
+		itr = itr->arr[c];
+	}
+
+	return prev->val;
 }
