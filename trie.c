6852be81be036494d2a7020851b0e61cc01750e0
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-06-27 23:49:26 +0530

    updated

--- trie.c
+++ trie.c
@@ -14,15 +14,14 @@ struct trie_struct {
 	int val;
 };
 
-trie_t* create_trie();
-trie_t* destroy_trie();
-int insert_key(trie_t* t, const char* key, int val);
+trie_t *create_trie();
+trie_t *destroy_trie();
+int insert_key(trie_t *t, const char *key, int val);
 
-int main(int argc, char **argv) {
-
-}
+int main(int argc, char **argv) {}
 
 trie_t *create_trie() {
+
 	trie_t *t = malloc(sizeof(*t));
 	t->is_terminal = 0;
 	t->val = 0;
@@ -34,20 +33,25 @@ trie_t *create_trie() {
 	return t;
 }
 
-int insert_key(trie_t* t, const char* key, int val){
-    if(!t){
-        fprintf(stderr, "null value for trie\n");
-        return -1;
-    }
+int insert_key(trie_t *t, const char *key, int val) {
+	if (!t) {
+		fprintf(stderr, "null value for trie\n");
+		return -1;
+	}
 
-    int len = strlen(key);
+	int len = strlen(key);
 
-    trie_t* cur_trie = t;
-    for(int i=0; i<len; i++){
-        int c = key[i];
-        if(!t->arr[c]){
-            trie_t* new_trie = init_trie();
-            new_trie->
-        }
-    }
+	trie_t *cur_trie = t;
+
+	for (int i = 0; i < len; i++) {
+		int c = key[i];
+		if (!cur_trie->arr[c]) {
+			cur_trie->arr[c] = create_trie();
+		}
+
+		cur_trie = cur_trie->arr[c];
+	}
+
+	cur_trie->is_terminal = 1;
+	cur_trie->val = val;
 }
