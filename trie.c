84689d5f7b1ea6aa40632217140dec0c50a92afb
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-06-28 08:29:38 +0530

    added sorting

--- trie.c
+++ trie.c
@@ -18,9 +18,23 @@ trie_t *create_node();
 int destroy_trie(trie_t *t);
 void insert_key(trie_t *t, const char *key);
 int search_key(trie_t *t, const char *key);
+void print_keys(trie_t* t, char* buff, int i);
 
 int main(int argc, char **argv) {
-	
+	trie_t* node = create_node();
+	insert_key(node,"manan");
+	insert_key(node,"man");
+	insert_key(node,"mano");
+	insert_key(node,"prakash");
+	insert_key(node,"prakish");
+	insert_key(node,"lmao");
+	insert_key(node,"ablu");
+	insert_key(node,"bablu");
+
+	char buff[100];
+	memset(buff,0,sizeof(buff));
+	print_keys(node, buff, 0);
+	destroy_trie(node);
 }
 
 trie_t *create_node() {
@@ -77,4 +91,17 @@ int destroy_trie(trie_t *t) {
 	free(t);
 	t = NULL;
 	return 0;
+}
+
+void print_keys(trie_t* t, char* buff, int j){
+	if(t->val){
+		buff[j]='\0';
+		printf("%s\n",buff);
+	}
+	for(int i=0; i<26; i++){
+		if(t->arr[i]){
+			buff[j] = 'a' + i;
+			print_keys(t->arr[i],buff,j+1);
+		}
+	}
 }
